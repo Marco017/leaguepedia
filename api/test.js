ba86fbd6557f537c94571ccf48ea00bcbd39a5f6
marco017 <marcorsfrancisco06@gmail.com>
2026-05-03 22:40:55 +0100

    initial commit

---
+++ api/test.js
@@ -0,0 +1,13 @@
+const champions = require('./db.json').champions;
+
+var tagsList = [];
+
+for (var i in champions) {
+    var tags = champions[i]?.tags || [];
+    for (var j in tags) { 
+        if (!tagsList.includes(tags[j])) {
+            tagsList.push(tags[j]);
+        }   
+    }
+}
+console.log(tagsList);
