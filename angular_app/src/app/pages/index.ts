98a82ee2dfbd26674118b93f9d67a9191219ea6e
Marco Francisco <156084118+Marco017@users.noreply.github.com>
2026-05-19 12:45:19 +0100

    Add pages/index.ts for re-exports"

---
+++ angular_app/src/app/pages/index.ts
@@ -0,0 +1,4 @@
+// Re-export champion-list component from app root
+// This file allows importing from 'app/pages/champion-list' while files are in app root
+export { ChampionListComponent } from '../champion-list.component';
+export { PaginationComponent } from '../pagination.component';
